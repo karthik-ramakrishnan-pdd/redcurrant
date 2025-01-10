a84dac0ef93e129286194b95ee98ef6ed7d512ac
pranav rk <pranav.ramakrishnan@pearldatadirect.com>
2025-01-10 15:27:31 +0530

    Solace Listener changes - Pranav

--- application/src/main/java/com/pdd/redcurrant/application/listeners/SolaceListener.java
+++ application/src/main/java/com/pdd/redcurrant/application/listeners/SolaceListener.java
@@ -18,7 +18,10 @@ public class SolaceListener {
 
     private final SolaceServicePort solaceService;
 
-    @JmsListener(destination = "${solace.topic.test.async}")
+    @Value("${solace.queue.test.sync}")
+    private String syncQueueName;
+
+    @JmsListener(destination = "${solace.queue.test.async}")
     public void handleAsync(String message) {
         try {
             log.info("Received: {}", message);
@@ -29,8 +32,9 @@ public class SolaceListener {
         }
     }
 
-    @JmsListener(destination = "${solace.topic.test.sync}")
+    @JmsListener(destination = "${solace.queue.test.sync}")
     public void handleSync(Message message, Session session) {
+        log.info("Listener invoked for queue: {}", syncQueueName);
         try {
             if (message instanceof TextMessage textMessage) {
                 String json = textMessage.getText();
@@ -46,8 +50,17 @@ public class SolaceListener {
                 session.createProducer(message.getJMSReplyTo()).send(responseMessage);
             }
         }
-        catch (JMSException ex) {
+        catch (Exception ex) {
             log.error("Error processing message: {}", ex.getMessage(), ex);
+
+            try {
+                var responseMessage = session
+                        .createTextMessage("Error Processing Data");
+                responseMessage.setJMSCorrelationID(message.getJMSCorrelationID());
+                session.createProducer(message.getJMSReplyTo()).send(responseMessage);
+            } catch (JMSException rollbackEx) {
+                log.error("Error with JMS session: {}", rollbackEx.getMessage(), rollbackEx);
+            }
         }
     }
 
